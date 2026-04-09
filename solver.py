ccd728b959350dbf8c5c9d6e4985b9deb486b6f1
Rafael Rodrigues <rodrigues2005rafa@gmail.com>
2026-04-09 15:15:37 +0100

    bfs done

--- solver.py
+++ solver.py
@@ -2,15 +2,32 @@ from state import LightsOutState
 
 
 def solve_bfs(initial_state):
-    """
-    Solve the board using Breadth-First Search.
+    if initial_state.is_goal():
+        return []
+    
+    #queue guarda tuplos com estado atual e caminho (usado deque para facilitar remoção de elementos)
+    queue = deque([(initial_state, [])])
+    
+    #funciona com set graças ao hash e eq 
+    visited = set([initial_state])
 
-    Args:
-        initial_state: LightsOutState
+    while queue:
+        current_state, path = queue.popleft()
 
-    Returns:
-        List of (row, col) moves that lead to the goal, or None if unsolvable.
-    """
+        for next_state, move in current_state.get_neighbors():
+            if next_state not in visited:
+                
+                #o prox passo é a solução
+                if next_state.is_goal():
+                    return path + [move]
+                
+                visited.add(next_state)
+                queue.append((next_state, path + [move]))
+                
+    return None
+
+
+def solve_dfs(initial_state):
     pass
 
 
